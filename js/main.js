2134acd3cbda264928c030ba499a8bcf84fe593f
coco <enricogunawan27@gmail.com>
2019-04-11 20:48:23 +0700

    Add component navbar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
+  el: "#app",
+  data: {
+    loggedIn: false
+  },
+
+  computed: {},
+
+  watch: {},
+
+  created() {},
+
+  methods: {
+    loginUser() {
+      this.loggedIn = true;
+    },
+
+    logoutUser() {
+      this.loggedIn = false;
     }
-})
+  }
+});
