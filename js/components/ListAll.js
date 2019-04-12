d8185abf15dcd3b6e30ce372c292d422f574ede4
fender13 <ej.fender@outlook.com>
2019-04-12 11:21:37 +0700

    card content readyto use

--- js/components/ListAll.js
+++ js/components/ListAll.js
@@ -1,24 +1,24 @@
 Vue.component('image-row', {
     template: `
-    <div>
 
-    <div class="row">
-        <figure class="effect-zoe">
-            <img src="https://static.pexels.com/photos/10664/b4d29be0fa6c806b5239556952d2e24d-large.jpg" alt="img26" />
-            <figcaption>
-                <h2>Creative <span>Zoe</span></h2>
-                <p class="icon-links">
-                    <a href="#"><span class="icon-heart"></span></a>
-                    <a href="#"><span class="icon-eye"></span></a>
-                    <a href="#"><span class="icon-paper-clip"></span></a>
-                </p>
-                <p class="description">Zoe never had the patience of her sisters. She deliberately punched the bear in his face.</p>
-            </figcaption>
-        </figure>
+    <div class="row container">
+        <div class="Instagram-card">
+            <div class="Instagram-card-header">
+                <p>User Name</p>
+            </div>
+        
+            <div class="Instagram-card-image">
+            <img src= "https://scontent-mia1-2.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/16123953_1838454153060984_5963456318336401408_n.jpg"/>
+            </div>
+
+            <div class="sharethis-inline-share-buttons" data-url="http://google.com" data-title="Look at this awesome picture!"></div>
+
+            <div class="Instagram-card-content well text-center">
+                <p>Test</p>
+                <hr>
+            </div> 
+      </div>
     </div>
 
-
-
-    </div>
     `
 })
